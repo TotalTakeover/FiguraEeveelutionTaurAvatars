040eabea22d8c42c1b09ab3c82e887215dc3ac65
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-03-21 04:09:36 -0700

    Spacing

--- scripts/TypeOrigins.lua
+++ scripts/TypeOrigins.lua
@@ -14,7 +14,7 @@ function events.TICK()
 	
 	if typeData.origin then
 		for _, v in ipairs(typeData.types) do
-			if typeData.tarString ~= v and originAPI.hasOrigin(player,  "eeveelutiontaurs:"..v.."taur") then
+			if typeData.tarString ~= v and originAPI.hasOrigin(player, "eeveelutiontaurs:"..v.."taur") then
 				
 				-- Update type
 				typeData:setTarget(typeData:getIndex(v))
